c7c9330095b9911adadef87db715bf6745edc572
Harish Sonawane <harish.sonawane0808@gmail.com>
2025-10-15 21:12:47 +0530

    new whatsapp message feature

---
+++ script.js
@@ -0,0 +1,15 @@
+function sendToWhatsApp(event) {
+  event.preventDefault();
+  
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const phoneNumber = "919321296327"; // Your WhatsApp number (with country code, no +)
+  const text = `New message from portfolio website:%0A
+  Name: ${name}%0A
+  Email: ${email}%0A
+  Message: ${message}`;
+
+  window.open(`https://wa.me/${phoneNumber}?text=${text}`, '_blank');
+}
